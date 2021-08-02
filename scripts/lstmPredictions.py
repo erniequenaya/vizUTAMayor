c987114db8b2172618b93a6d0bde344e7cad233a
seishi1 <major_seishi@hotmail.com>
2021-08-02 15:30:31 -0400

    subiendo modelo random forest

---
+++ scripts/lstmPredictions.py
@@ -0,0 +1,154 @@
+#!/root/prediccionService/venv/bin/python
+# Predictor maker
+# To be run at least one hour after retraining
+import os
+import datetime
+#import matplotlib as mpl
+#import matplotlib.pyplot as plt
+import numpy as np
+import pandas
+import tensorflow as tf
+import sqlalchemy
+import pymysql
+#import mysql.connector as connection
+#from sklearn.preprocessing import MinMaxScaler
+
+try:
+    # para rescatar las ultimas 24 horas: 24 * 60 * 12
+    engine = sqlalchemy.create_engine('mysql+pymysql://:@192.168.50.176/weather')
+    mydb = engine.connect()
+    #stackPreds = pandas.read_sql(query,mydb)
+    query = "SELECT * FROM WEATHER_MEASUREMENT ORDER BY ID DESC LIMIT 17280;"
+    # el ultimo dia solo hubo 5442 registros q no alcanzan pa 24 horas
+    df = pandas.read_sql(query,mydb)
+except exception as e:
+    mydb.close() #close the connectionexcept Exception as e:
+    mydb.close()
+    print(str(e))
+
+df = df.iloc[::-1] # dando vuelta el dataframe
+df["utc"] = pandas.to_datetime(df["serverDate"],format='%Y-%m-%d %H:%M:%S')
+df = df[['AMBIENT_TEMPERATURE','AIR_PRESSURE','HUMIDITY','utc']]
+# generando variables de tiempo para alimentar los modelos
+# df['minutes'] = pandas.to_datetime(df['utc']).dt.minute
+df['hour'] = pandas.to_datetime(df['utc']).dt.hour
+df['day'] = pandas.to_datetime(df['utc']).dt.day
+df['month'] = pandas.to_datetime(df['utc']).dt.month
+# agrupando parametros segun hora para obtener un dataset de 24 filas
+df2 = df.groupby('hour').mean()
+df2['hour'] = df2.index
+row = df2.mean()
+
+# basta con que se genere 1 registro cada una hora para poder alimentar el modelo
+##### solo por ahora, rellenamos los periodos de las ultimas 24 horas faltantes con promedios de los otros registros ######
+df3 = pandas.DataFrame(columns=row.keys())
+for i in range(24):
+    if i in df2.hour:
+        print(i)
+    else:
+        row.hour = i
+        df3 = df3.append(row,ignore_index=True)
+
+df2 = df2.append(df3)
+df2 = df2.sort_values('hour')
+df2 = df2.reset_index(drop=True)
+#df2.plot(subplots=True)
+#plt.show()
+##### solo por ahora, rellenamos los periodos de las ultimas 24 horas faltantes con promedios de los otros registros ######
+
+# Normalizando manualmente
+# el modelo fue entrenado con valores normalizados generados en base a metricas -promedio, desviacion estandar- del dataset de 
+# entrenamiento, por lo tanto, estas metricas se traspasan a los predictandos(?) actuales para respetar los valores
+# con los que fue entrenado el modelo
+
+#modelMean = {'AMBIENT_TEMPERATURE': 18.833937942048827, 'HUMIDITY': 67.28831089816715, 'AIR_PRESSURE': 1009.0120921743098, 'hour': 11.500418282759146, 'day': 15.734086242299794, 'month': 6.521370446421781}
+#modelStd = {'AMBIENT_TEMPERATURE': 3.0178459228432164, 'HUMIDITY': 7.216205483706377, 'AIR_PRESSURE': 2.2937924094103446, 'hour': 6.92221927192334, 'day': 8.801762436194657, 'month': 3.448822871574395}
+modelMean = [18.833937942048827, 67.28831089816715, 1009.0120921743098, 11.500418282759146, 15.734086242299794, 6.521370446421781]
+modelStd = [3.0178459228432164, 7.216205483706377, 2.2937924094103446, 6.92221927192334, 8.801762436194657, 3.448822871574395]
+# you can straight substract a list from dataframe without giving key values!
+df3 = df2[['AMBIENT_TEMPERATURE','HUMIDITY','AIR_PRESSURE','hour','day','month']]
+df3 = (df3 - modelMean) / modelStd
+df3 = df3.to_numpy()
+# reshape a valores actuales
+df3 = np.reshape(df3,(-1,24,6))
+# df3 just became a 3 dimensional array jesus...
+
+# Cargado de modelos, 1 por variable
+tsModel = tf.keras.models.load_model('../dense+lstmMnormTs/')
+qfeModel = tf.keras.models.load_model('../dense+lstmMnormQFE/')
+#hrModel = tf.keras.models.load_model('./dense+lstmHRv2/') # to load with a proper model
+
+#x = tsModel.predict(df3) # array([[0.0564339]], dtype=float32), meanAsDict -0.5708247, meanAsList 1.0230861
+#y = qfeModel.predict(df3) # array([[2.6169283]], dtype=float32), meanAsDict 2.0415623, meanAsList -2.3020828
+#z = 1.5
+#########
+#x = x*modelStd[0]+modelMean[0]
+
+# Implementando funcion normalizadora
+def norm(value,index):
+    value = (value - modelMean[index]) / modelStd[index]
+    return value
+
+# implementando autoregresion (windowing system manual)
+
+#norm(now.hour,3)
+#df4 = np.array([x,y,z,now.hour,now.day,now.month],dtype="float32")
+#df4 = np.array([x,y,z,norm(now.hour,3),norm(now.day,4),norm(now.month,5)],dtype="float32")
+now = datetime.datetime.now()
+stackPreds = pandas.DataFrame()
+x = tsModel.predict(df3)
+y = 1.5
+z = qfeModel.predict(df3)
+#stackPreds = pandas.DataFrame(columns=row.keys())
+# para predecir mas horas basta con cambiar 12 a 168
+for i in range(0,72):
+    delta = now + datetime.timedelta(0,i*3600)
+    #temp = np.array([x,y,z,delta.hour,delta.day,delta.month],dtype="float32")
+    temp = np.array([x,y,z,norm(delta.hour,3),norm(delta.day,4),norm(delta.month,5)],dtype="float32")
+    stackPreds = stackPreds.append(pandas.DataFrame(temp).transpose())
+    # df3 = np.vstack((df3[0],temp))
+    # df4 = np.vstack((df4,temp))
+    cde = np.reshape(df3,(24,6))
+    df3 = np.vstack((cde,temp))
+    df3 = np.delete(df3, (0), axis=0)
+    df3 = np.reshape(df3,(-1,24,6))
+    x = tsModel.predict(df3)
+    y = 1.5
+    z = qfeModel.predict(df3)
+
+#modelStd[0]+modelMean[0]
+
+#plt.plot(df4[0,0:24,0])
+#plt.plot(df4[0,0:24,1])
+#plt.plot(df4[0,0:24,2])
+#plt.grid()
+#plt.show()
+
+stackPreds.columns = ['AMBIENT_TEMPERATURE','HUMIDITY','AIR_PRESSURE','hour','day','month']
+stackPreds = stackPreds.reset_index()
+stackPreds = stackPreds[['AMBIENT_TEMPERATURE','HUMIDITY','AIR_PRESSURE','hour','day','month']]
+
+#stackPreds = stackPreds.reset_index()
+
+# de-normalizando predicciones
+stackPreds=stackPreds*modelStd+modelMean
+# stackPreds.plot(subplots=True)
+# plt.show()
+# plt.plot(stackPreds['AMBIENT_TEMPERATURE'])
+
+stackPreds.AMBIENT_TEMPERATURE = stackPreds.AMBIENT_TEMPERATURE.round(3)
+stackPreds.HUMIDITY = stackPreds.HUMIDITY.round(3)
+stackPreds.AIR_PRESSURE = stackPreds.AIR_PRESSURE.round(3)
+stackPreds.hour = stackPreds.hour.round(0)
+stackPreds.day = stackPreds.day.round(0)
+stackPreds.month = stackPreds.month.round(0)
+
+try: 
+    stackPreds.to_sql('predictions',mydb,if_exists='replace',index=False)
+except:
+    print("error en conexion a db")
+
+#print(stackPreds)
+mydb.close()
+engine.dispose()
+
---
+++ scripts/rfPredictions.py
@@ -0,0 +1,93 @@
+
+import os
+import datetime
+#import matplotlib as mpl
+#import matplotlib.pyplot as plt
+import numpy as np
+import pandas
+#import tensorflow as tf
+import sqlalchemy
+import pymysql
+#from sklearn.ensemble import RandomForestRegressor
+import joblib
+
+def createTimeFeatures (dfToExpand):
+    # A pesar de estar definida como una funcion que crea y retorna una copia del dataset que se le es proveida
+    # aun así el dataset original es modificado, no se por que
+    dfToExpand['hour'] = pandas.to_datetime(dfToExpand['utc']).dt.hour
+    dfToExpand['day'] = pandas.to_datetime(dfToExpand['utc']).dt.day
+    dfToExpand['month'] = pandas.to_datetime(dfToExpand['utc']).dt.month
+    dfToExpand['year'] = pandas.to_datetime(dfToExpand['utc']).dt.year
+    return dfToExpand
+
+try:
+    engine = sqlalchemy.create_engine('mysql+pymysql:// --- : --- @192.168.50.176/weather')
+    mydb = engine.connect()
+    # Obteniendo ultimos 720 (60*12) registros, equivalentes a una hora de datos o mas, en caso de que haya faltantes
+    # y evitar lecturas fluctuantes
+    query = "SELECT * FROM WEATHER_MEASUREMENT ORDER BY ID DESC LIMIT 16720;"
+    df = pandas.read_sql(query,mydb)
+except:
+    mydb.close() #close the connectionexcept Exception as e:
+    print('Error en conexion a base de datos')
+
+# Inversion del dataframe 
+df = df.iloc[::-1] 
+df["utc"] = pandas.to_datetime(df["serverDate"],format='%Y-%m-%d %H:%M:%S')
+# Por ahora se ignora id de sensor
+df = df[['AMBIENT_TEMPERATURE','HUMIDITY','AIR_PRESSURE','utc']]
+# Generando variables de tiempo para alimentar el modelo
+df = createTimeFeatures(df)
+train_dates = df.pop('utc')
+# Agrupando parametros segun hora para obtener un dataset de 24 filas
+df2 = df.groupby('hour').mean()
+df2['hour'] = df2.index
+row = df2.mean()
+
+# El modelo random forest al no ser entrenado con valores normalizados no necesita que los nuevos valores a evaluar tengan este tratamiento
+# Si el orden de los datos debe ser el mismo
+# Temperatura - Humedad - Presion - hora - dia - mes - año
+df2 = df2[['AMBIENT_TEMPERATURE','HUMIDITY','AIR_PRESSURE','hour','day','month','year']]
+
+# Carga de modelo
+# Caracteristicas: Input: 7 variables, Output: 3 variables, Predicciones: Pasos de 1 hora
+rfMultivar=joblib.load('../pyRF/rf-multivarOffset.joblib')
+
+x = rfMultivar.predict(df2[1:])
+
+now = datetime.datetime.now()
+stackPreds = pandas.DataFrame()
+#stackPreds = pandas.DataFrame(columns=row.keys())
+# para predecir mas horas basta con cambiar 72 a un valor mas alto
+for i in range(0,72):
+    delta = now + datetime.timedelta(0,i*3600)
+    #temp = np.array([x,y,z,delta.hour,delta.day,delta.month],dtype="float32")
+    temp = np.array([x[0,0],x[0,1],x[0,2],delta.hour,delta.day,delta.month,delta.year],dtype="float32")
+    stackPreds = stackPreds.append(pandas.DataFrame(temp).transpose())
+    # Formateando registro para alimentar el modelo
+    temp_RE = np.reshape(temp,(-1,7))
+    x = rfMultivar.predict(temp_RE)
+
+stackPreds = stackPreds.reset_index(drop=True)
+#stackPreds.plot(subplots=True)
+#plt.show()
+
+# Formateo de datos para subida a base de datos
+stackPreds.columns = ['AMBIENT_TEMPERATURE','HUMIDITY','AIR_PRESSURE','hour','day','month','year']
+
+stackPreds.AMBIENT_TEMPERATURE = stackPreds.AMBIENT_TEMPERATURE.round(3)
+stackPreds.HUMIDITY = stackPreds.HUMIDITY.round(3)
+stackPreds.AIR_PRESSURE = stackPreds.AIR_PRESSURE.round(3)
+stackPreds.hour = stackPreds.hour.round(0)
+stackPreds.day = stackPreds.day.round(0)
+stackPreds.month = stackPreds.month.round(0)
+
+try:
+    stackPreds.to_sql('rfPredictions',mydb,if_exists='replace',index=False)
+except:
+    mydb.close() #close the connectionexcept Exception as e:
+    print('Error en conexion a base de datos')
+
+mydb.close()
+engine.dispose()
+## script ready to be callbablew
