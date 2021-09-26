077e4cc9363385608d9170bdf3da37210b18ba99
seishi1 <major_seishi@hotmail.com>
2021-09-26 20:04:51 -0300

    limpiando carpeta del proyecto

--- scripts/reTrain/arimaRetrain.py
+++ scripts/reTrain/arimaRetrain.py
@@ -1,3 +1,6 @@
+# este codigo es solo de muestra, puesto que no existe un modelo ARIMA entrenado el cual sea llamado cada 24 horas para realizar predicciones de forma regular
+# en cambio, cada 24 horas se construyen modelos ARIMA nuevos para realizar las predicciones
+
 import os
 import datetime
 #import matplotlib.pyplot as plt
@@ -9,7 +12,7 @@ from statsmodels.tsa.arima.model import ARIMA
 from sklearn.metrics import mean_absolute_error
 #import joblib
 
-df = pandas.read_csv("./dataPreprocessed.csv",parse_dates=['utc'],index_col=['utc'])
+df = pandas.read_csv("../dataPreprocessed.csv",parse_dates=['utc'],index_col=['utc'])
 # Manteniendo últimos dos dias de registros
 lendf = len(df)
 df = df[-148:-72]
@@ -17,7 +20,6 @@ df = df.interpolate(method="linear")
 series = df.copy()
 series.index = series.index.to_period('H')
 
-#este codigo no es ejecutable por un bug de la version importada de statsmodels la cual esta desactualizada (pero que es la funcional para google colab)
 future = 72
 
 stackPreds = pandas.DataFrame()
@@ -31,7 +33,7 @@ model = ARIMA(series['QFE_Valor'], order=(24,1,0))
 model_fit = model.fit()
 stackPreds['QFE_Valor'] = model_fit.forecast(future)
 
-df = pandas.read_csv("./dataPreprocessed.csv",parse_dates=['utc'],index_col=['utc'])
+df = pandas.read_csv("../dataPreprocessed.csv",parse_dates=['utc'],index_col=['utc'])
 test_labels = df[-72:]
 a = mean_absolute_error(stackPreds[['Ts_Valor','HR_Valor','QFE_Valor']],test_labels[['Ts_Valor','HR_Valor','QFE_Valor']],multioutput='raw_values')
 a
