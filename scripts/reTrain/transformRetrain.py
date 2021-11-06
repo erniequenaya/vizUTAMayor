5a33f37611dfb74f70ebc6d94369ee463cb62ea5
seishi1 <major_seishi@hotmail.com>
2021-11-06 15:34:55 -0300

    pusheando script de predicciones mediante transformer

--- scripts/reTrain/transformRetrain.py
+++ scripts/reTrain/transformRetrain.py
@@ -2,8 +2,8 @@
 
 import os
 import datetime
-import matplotlib as mpl
-import matplotlib.pyplot as plt
+#import matplotlib as mpl
+#import matplotlib.pyplot as plt
 import numpy as np
 import pandas
 import tensorflow as tf
@@ -31,11 +31,8 @@ df['ysin']=timeEncode['Year sin']
 
 ## Dividiendo dataset
 n = len(df)
-#train_df = df[0:int(n*0.7)]
 train_df = df[0:int(n*0.8)]
-#val_df = df[int(n*0.7):int(n*0.9)]
 val_df = df[int(n*0.8):int(n*0.95)]
-#test_df = df[int(n*0.9):]
 test_df = df[int(n*0.95):]
 
 num_features = df.shape[1]
@@ -130,22 +127,21 @@ class WindowGenerator():
     return result
 
 
-## Testeando sistema de ventanas
+## Definiendo parametros para sistema de ventanas del modelo
+## El modelo será de tipo secuencia a secuencia de 24 horas de últimos datos a 24 horas de predicciones
 winSize=24
 winLabel = 24
 wide_window = WindowGenerator(
     #input_width=winSize, label_width=1, shift=1, # autoregresivo puro
     input_width=winSize, label_width=winLabel, shift=24, # ventanas de 24 horas
     label_columns=['Ts_Valor','HR_Valor','QFE_Valor','dsin','ysin'])
-    #label_columns=['Ts_Valor','HR_Valor','QFE_Valor','ysin','ycos'])
 
-
-wide_window
-
+###Testo unicamente
+#wide_window
 for example_inputs, example_labels in wide_window.train.take(1):
   print(f'Inputs shape (batch, time, features): {example_inputs.shape}')
   print(f'Labels shape (batch, time, features): {example_labels.shape}')
-
+###
 
 ## Implementando algoritmo transformer adoptado a series de tiempo, proveido por keras 
 # [https://keras.io/examples/timeseries/timeseries_transformer_classification/]
@@ -164,8 +160,8 @@ def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
 
     # Feed Forward Part
     x = layers.LayerNormalization(epsilon=1e-6)(res)
-    #x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(x)
-    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation=tf.nn.leaky_relu)(x)
+    #x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(x) #todas las activaciones de capa son cambiadas a leaky_relu para maximizar la
+    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation=tf.nn.leaky_relu)(x) #extraccion de aprendizaje de los datos
     x = layers.Dropout(dropout)(x)
     #x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
     x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1, activation=tf.nn.leaky_relu)(x)
@@ -187,8 +183,8 @@ def build_model(
     for _ in range(num_transformer_blocks):
         x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
 
-    #x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
-    x = layers.Flatten()(x)
+    #x = layers.GlobalAveragePooling1D(data_format="channels_first")(x) #Se cambia el promediado global por una capa flatten simple para no 
+    x = layers.Flatten()(x) # introducir "smoothing" al modelo
     for dim in mlp_units:
         x = layers.Dense(dim, activation="relu")(x)
         x = layers.Dropout(mlp_dropout)(x)
@@ -219,54 +215,58 @@ trans.add(model)
 trans.add(tf.keras.layers.Dense(winLabel*num_features,activation="linear"))
 trans.add(tf.keras.layers.Reshape([-1, num_features]))
 
-MAX_EPOCHS=3
+MAX_EPOCHS=7
 ## Desde la version 2.5 en adelante de tensorflow, tf ya no provee optimizadores de forma independiente
 ## sino que lo hace a traves de Keras -que al fin y al cabo viene integrado en tf. De esta forma solo cambia la ruta para referencias los optimizadores
 #trans.compile(loss=tf.losses.MeanSquaredError(), optimizer=tf.optimizers.Adam(), metrics=[tf.metrics.MeanAbsoluteError()])
 trans.compile(loss=tf.keras.losses.MeanSquaredError(), optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4), metrics=[tf.keras.metrics.MeanAbsoluteError()])
 trans.fit(wide_window.train, epochs=MAX_EPOCHS, validation_data=wide_window.val,shuffle=False)
 
-## Testeando, transferir codigo a trnsfmPredictions
-df3 = val_df[-winSize:]
+# Salvando modelo
+trans.save("../../../models/transformer")
 
-df3 = test_df[300:324]
-df3 = test_df[600:624]
-stackPreds = pandas.DataFrame()
-df3 = df3.to_numpy()
-df3 = df3.reshape((-1,winSize,num_features))
-df3.shape
-
-#now = pandas.to_datetime("2021-04-28 16:00:00")
-#now = pandas.Timestamp.timestamp(now)
-x = trans.predict(df3)
-a = trans.predict(x)
-b = trans.predict(a)
-x = np.hstack((x,a))
-x = np.hstack((x,b))
-
-
-## Como el modelo genera outputs del mismo tipo que su input, puede implementarse de forma semi-autoregresiva,
-## pues en vez de iria hora->hora va dia->dia
-x
-
-#stackPreds = pandas.DataFrame(np.reshape(x,(winLabel,num_features)))
-stackPreds = pandas.DataFrame(np.reshape(x,(72,num_features)))
-stackPreds.columns=['Ts_Valor','HR_Valor','QFE_Valor','dsin','ysin']
-#stackPreds.columns=['Ts_Valor','HR_Valor','QFE_Valor','dsin','ysin','ycos']
-#stackPreds.columns=['Ts_Valor','HR_Valor','QFE_Valor','ysin','ycos']
-
-test_df = test_df.reset_index(drop=True)
-first = 600
-last = first+72
-test_sample = test_df[first:last]
-test_sample = test_sample.reset_index(drop=True)
-
-#plt.plot(test_sample['Ts_Valor'], label='testLabels')
-#plt.plot(stackPreds['Ts_Valor'],label='stackPreds')
-#plt.legend()
-#plt.grid()
-#plt.show()
+###### Adjuntando código para testeo de modelo como anexo
+### Testeando, transferir codigo a trnsfmPredictions
+#df3 = val_df[-winSize:]
 #
+#df3 = test_df[300:324]
+#df3 = test_df[600:624]
+#stackPreds = pandas.DataFrame()
+#df3 = df3.to_numpy()
+#df3 = df3.reshape((-1,winSize,num_features))
+#df3.shape
+#
+##now = pandas.to_datetime("2021-04-28 16:00:00")
+##now = pandas.Timestamp.timestamp(now)
+#x = trans.predict(df3)
+#a = trans.predict(x)
+#b = trans.predict(a)
+#x = np.hstack((x,a))
+#x = np.hstack((x,b))
+#
+#
+### Como el modelo genera outputs del mismo tipo que su input, puede implementarse de forma semi-autoregresiva,
+### pues en vez de iria hora->hora va dia->dia
+#x
+#
+##stackPreds = pandas.DataFrame(np.reshape(x,(winLabel,num_features)))
+#stackPreds = pandas.DataFrame(np.reshape(x,(72,num_features)))
+#stackPreds.columns=['Ts_Valor','HR_Valor','QFE_Valor','dsin','ysin']
+##stackPreds.columns=['Ts_Valor','HR_Valor','QFE_Valor','dsin','ysin','ycos']
+##stackPreds.columns=['Ts_Valor','HR_Valor','QFE_Valor','ysin','ycos']
+#
+#test_df = test_df.reset_index(drop=True)
+#first = 600
+#last = first+72
+#test_sample = test_df[first:last]
+#test_sample = test_sample.reset_index(drop=True)
+#
+##plt.plot(test_sample['Ts_Valor'], label='testLabels')
+##plt.plot(stackPreds['Ts_Valor'],label='stackPreds')
+##plt.legend()
+##plt.grid()
+##plt.show()
+##
 #plt.plot(test_sample['HR_Valor'], label='testLabels')
 #plt.plot(stackPreds['HR_Valor'],label='stackPreds')
 #plt.legend()
@@ -290,10 +290,9 @@ test_sample = test_sample.reset_index(drop=True)
 #plt.legend()
 #plt.grid()
 #plt.show()
-
-from sklearn.metrics import mean_absolute_error
-a = mean_absolute_error(stackPreds[['Ts_Valor','HR_Valor','QFE_Valor']],test_df[['Ts_Valor','HR_Valor','QFE_Valor']][first:last],multioutput='raw_values')
-a
-
-trans.save("../../../models/transformer")
-# array([0.51061708, 0.61037691, 0.64611513]) 24->24 to 72 dsin+ysin leakyrelu
+#
+#from sklearn.metrics import mean_absolute_error
+#a = mean_absolute_error(stackPreds[['Ts_Valor','HR_Valor','QFE_Valor']],test_df[['Ts_Valor','HR_Valor','QFE_Valor']][first:last],multioutput='raw_values')
+#a
+#
+## array([0.51061708, 0.61037691, 0.64611513]) 24->24 to 72 dsin+ysin leakyrelu
